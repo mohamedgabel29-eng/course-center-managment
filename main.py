eaa7d0ab826b4898d602e8be769b856d9cac3ebb
Mohamed Gabal <mohamedgabel29@gmail.com>
2026-06-25 03:12:40 +0300

    more enhanced on "main file"

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ courses = []
 ADMIN_NAME = "Eng.Mohamed"
 ADMIN_ID = "162007"
 name = input("Enter admin name: ")
-admin_id = input("Enter admin id: ")
+admin_id = int(input("Enter admin id: "))
 
 if name == ADMIN_NAME and admin_id == ADMIN_ID:
     print("Login successful")
@@ -23,12 +23,10 @@ else:
 # menu
 while True:
     print("1. Add student")
-    print("2. Add instructor")
-    print("3. Add course")
-    print("4. Display students")
-    print("5. Display instructors")
-    print("6. Display courses")
-    print("7. Exit")
+    print("2. Display students")
+    print("3. Display instructors")
+    print("4. Display courses")
+    print("5. Exit")
     choice = input("Enter your choice: ")
     if choice == "1":
 
@@ -54,33 +52,22 @@ while True:
             student = Student(name, age, ID)
             students.append(student)
             print("Student added successfully.")
+    
     elif choice == "2":
-        name = input("Enter instructor name: ")
-        age = input("Enter instructor age: ")
-        ID = int(input("Enter instructor ID: "))
-        salary = float(input("Enter instructor salary: "))
-        instructor = Instructor(name, age, ID, salary)
-        instructors.append(instructor)
-    elif choice == "3":
-        title = input("Enter course title: ")
-        price = float(input("Enter course price: "))
-        course = Course(title, price)
-        courses.append(course)
-    elif choice == "4":
       if not students:
         print("No students found.")
       else:
         for student in students:
             student.display()
             print("-" * 20)
-    elif choice == "5":
+    elif choice == "3":
         for instructor in instructors:
             instructor.display()
-    elif choice == "6":
+    elif choice == "4":
         for course in courses:
             print(f"Title: {course.title}")
             print(f"Price: {course.price}")
-    elif choice == "7":
+    elif choice == "5":
         break 
     else:
         print("Invalid choice")
