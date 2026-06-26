c1cffcc0a25a6488eb222439815419490479f5e6
Mohamed Gabal <mohamedgabel29@gmail.com>
2026-06-26 03:18:59 +0300

    Refactor main.py and improve menu system

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ from course import Course
 
 students = []
 
-
-# instructor defnition
+# instructor objects
 ahmed = Instructor(
     "Ahmed Hassan",
     35,
@@ -30,7 +29,7 @@ haneen = Instructor(
     salary=20000
 )
 instructors = [ahmed, mohamed, haneen]  # List of instructor objects
-# course defnition
+# course objects
 python_course = Course(
     "Python Development",
     500,
@@ -55,39 +54,36 @@ ds_course = Course(
 """
 courses = [ml_course, python_course, ds_course]
 
-
 """Admin information
 """
 def admin_login():
     ADMIN_NAME = "Eng.Mohamed"
     ADMIN_ID = 162007
-
-    name = str(input("Enter admin name: "))
-    admin_id = int(input("Enter admin id: "))
+    try:
+     name = str(input("Enter admin name: "))
+     admin_id = int(input("Enter admin id: "))
+    except ValueError:
+        print("Invalid input. Please enter valid data.")
+        return False
     if name == ADMIN_NAME and admin_id == ADMIN_ID:
         print("Login successful")
+        return True
     else:
         print("Access denied")
-        exit()
-        
-
-
-# menu function
-def show_menu():
-    print("welcome to Ai_Hero course center... please choose an option:")
-    print("1. Add student")
-    print("2. Display students")
-    print("3. search for courses")
-    print("4. enroll student in courses")
-    print("5. display courses")
-    print("6. Delete student")
-    print("7. Show Course Statistics")
-    print("8. Exit")
-# add student function 
+        return False
+             
+# ********************************************************
+# menu functions 
+# ********************************************************
+#1. add student function 
 def add_student():
-    name = str(input("Enter student name: "))
-    age = int(input("Enter student age: "))
-    ID = int(input("Enter student ID: "))
+    try:
+     name = str(input("Enter student name: "))
+     age = int(input("Enter student age: "))
+     ID = int(input("Enter student ID: "))
+    except ValueError:
+        print("Invalid input. Please enter valid data.") 
+        return
 
     found = False
 
@@ -108,7 +104,7 @@ def add_student():
         students.append(student)
         print("Student added successfully.")
       
-# display students function
+#2. display students function
 def display_students():
     if not students:
         print("No students found.")
@@ -117,14 +113,14 @@ def display_students():
             student.display()
             print("-" * 20)
             
-# search for courses function
+#3. search for courses function
 def search_courses():
+    
     search_name = str(input("Enter course name: "))
-
     found = False
 
     for course in courses:
-        if course.title.lower() == search_name.lower():
+         if course.title.lower() == search_name.lower():
 
             print(f"Course: {course.title}")
             print(f"Price: {course.price}")
@@ -135,12 +131,17 @@ def search_courses():
             found = True
             break
 
-        if not found:
+    if not found:
           print("Course not found.")
                 
-#enroll student in courses function
+#4. enroll student in courses function
 def enroll_student_in_course():
-    student_id = int(input("Enter student ID: "))
+    try:
+     student_id = int(input("Enter student ID: "))
+    except ValueError:
+        print("Invalid input. Please enter a number.")
+        return
+
     selected_student = None
 
     for student in students:
@@ -155,8 +156,11 @@ def enroll_student_in_course():
     print("\nAvailable Courses:")
     for index, course in enumerate(courses, start=1):
         print(f"{index}. {course.title}")
-
-    course_number = int(input("Choose course number: "))
+    try:    
+     course_number = int(input("Choose course number: "))
+    except ValueError:
+        print("Invalid input. Please enter a number.")
+        return
 
     if 1 <= course_number <= len(courses):
         selected_course = courses[course_number - 1]
@@ -168,75 +172,140 @@ def enroll_student_in_course():
             print("Student already enrolled in this course.")
     else:
         print("Invalid course number.")   
-# display courses function
+#5. display courses function
 def display_courses():
     for course in courses:
         print(f"Title: {course.title}")
         print(f"Price: {course.price}")
         print(f"Instructor: {course.Instructor.name}")
         print("-" * 20)                
-# Delete student function
+#6. Delete student function
 def delete_student():
-    student_id = int(input("Enter student ID to delete: "))
+    try:
+     student_id = int(input("Enter student ID to delete: "))
+    except ValueError:
+        print("Invalid input. Please enter a number.")
+        return
     found = False
 
     for student in students:
         if student.ID == student_id:
             students.remove(student)
             print("Student deleted successfully.")
+            print("*" * 20)
             found = True
             break
 
     if not found:
         print("Student not found.") 
-#Show Course Statistics function
+#7. Show Course Statistics function
 def show_course_statistics():
         for course in courses:
-         print("=" * 30)
-         print(course.title)
-         print()
+          print("=" * 30)
+          print(course.title)
+          print()
 
-        count = 0
-        for student in students:
+          count = 0
+          for student in students:
             if course in student.courses:
                 print(f"- {student.name}")
                 count += 1
-        print(f"\nTotal Students: {count}") 
-                        
-admin_login()
+          print(f"\nTotal Students: {count}") 
+          print("*" * 20)
+def admin_menu():
+    while True:
+        print("Welcome Admin, please choose an option:")
+        print("1. Display Students")
+        print("2. Search for Courses")
+        print("3. Display Courses")
+        print("4. Delete Student")
+        print("5. Show Course Statistics")
+        print("6. Exit")
+        try:
+            choice = input("Enter your choice: ")
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+            continue
 
-while True:
-    show_menu()
-    try:
-     choice = input("Enter your choice: ")
-    except ValueError:
-        print("Invalid input. Please enter a number.")
-        continue
+        if choice == "1":
+            display_students()
+            print("*" * 20)
+        elif choice == "2":
+            search_courses()
+            print("*" * 20)
+        elif choice == "3":
+            display_courses()
+            print("*" * 20)
+        elif choice == "4":
+            delete_student()
+            print("*" * 20)
+        elif choice == "5":
+            show_course_statistics()
+            print("*" * 20)
+        elif choice == "6":
+            print("Thank you for using the system.")
+            print("*" * 20)
+            break
+        else:
+            print("Invalid choice. Please try again.")
+# guest menu function
+def guest_menu():
+    while True:
+        print("Welcome Guest, please choose an option:")
+        print("1. add student")
+        print("2. search for  courses")
+        print("3. enroll student in course")
+        print("4. display courses")
+        print("5. Exit")
+        try:
+         choice = input("Enter your choice: ")
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+            continue
+
+        if choice == "1":
+            print("*" * 20)
+            add_student()
+            print("*" * 20)
+        elif choice == "2":
+            print("*" * 20)
+            search_courses()
+            print("*" * 20)
+        elif choice == "3":
+            print("*" * 20)
+            enroll_student_in_course() 
+            print("*" * 20)   
+        elif choice == "4":
+            print("*" * 20)
+            display_courses()
+            print("*" * 20)
+        elif choice == "5":
+            print("Thank you for using the system.")
+            break
+        else:
+            print("Invalid choice. Please try again.")              
+                      
+def main():                       
+ while True:
+    print("===== Welcome =====")
+    print("1. Login as Admin")
+    print("2. Continue as Guest")
+    print("3. Exit")
+    
+    choice = input("Enter your choice: ")
     
     if choice == "1":
-        add_student()
-    
-    elif choice == "2":  
-     display_students()
+
+        if admin_login():
+            admin_menu()
+
+    elif choice == "2":
+
+        guest_menu()
 
     elif choice == "3":
-
-        search_courses()
-        
-    elif choice == "4":
-        enroll_student_in_course()
-        
-    elif choice == "5":
-        display_courses()
-   
-    elif choice == "6":
-        delete_student()
-        
-    elif choice == "7":
-     show_course_statistics()
-                  
-    elif choice == "8":
-        break 
-    else:
-        print("Invalid choice")
-         
+        print("Thank you for using our system!")
+        break
+    
+if __name__ == "__main__":
+    main()          
